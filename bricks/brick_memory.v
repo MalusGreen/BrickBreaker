1d1d0e6a8d5521bf4bb53da1ab8406f0d68a0279
TootyManGrass <jerryx2468@gmail.com>
2016-11-27 16:33:57 -0500

    Removed dependence of clock from brick_memory.v

--- bricks/brick_memory.v
+++ bricks/brick_memory.v
@@ -1,25 +1,25 @@
+//`include "memory.v"
+//`include "address_xy.v"
+//`include "macros.v"
+
 module brick_memory(
 		input clk,
-		input resetn,
 		input [9:0]x_in, y_in,
 		input wren,
 		input [1:0]health_in,
 		
-		output reg [1:0]health,
-		output reg [9:0]x,
-		output reg [9:0]y
+		output [1:0]health,
+		output [9:0]x,
+		output [9:0]y
 	);
-	wire [1:0]data;
 	
 	wire [9:0]address;
 	
-	wire [9:0]x_carryout, y_carryout;
-	
 	address_xy memorychange(
 		.x_in(x_in),
-		.x_out(x_carryout),
+		.x_out(x),
 		.y_in(y_in),
-		.y_out(y_carryout),
+		.y_out(y),
 		.address_in(address),
 		.address_out(address)
 	);
@@ -29,20 +29,7 @@ module brick_memory(
 		.clock(clk),
 		.data(health_in),
 		.wren(wren),
-		.q(data)
+		.q(health)
 	);
-	
-	always @ (posedge clk)begin
-		if(!resetn)begin
-			health <= 0;
-			x <= 0;
-			y <= 0;
-		end
-		else begin
-			health <= data;
-			x = x_carryout;
-			y = y_carryout;
-		end
-	end
 	
 endmodule
