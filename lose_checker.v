6c7bb682b4add937db2e7059b7a03974f0c7cec8
TootyManGrass <jerryx2468@gmail.com>
2016-11-28 19:15:01 -0500

    Changed lose_checker to take in ball_y as an input.

--- lose_checker.v
+++ lose_checker.v
@@ -2,20 +2,22 @@ module lose_checker(
 	input clk,
 	input resetn,
 
-	input lost_health,
-	input [9:0]starting health,
+	input [9:0]ball_y,
+	input [9:0]starting_health,
 	
-	output loss_occurred
+	output reg loss_occurred
 	);
 	
 	reg [9:0]health_counter;
+	reg [9:0]previous_y;
 	
 	always @(posedge clk) begin
 		if(!resetn) begin
 			loss_occurred <= 10'd0;
 			health_counter <= starting_health;
+			previous_y <= BALLY;
 		end
-		else if(lost_health) begin
+		else if(ball_y > `PLATY & previous_y <= `PLATY) begin
 			if(health_counter == 10'd0) begin
 				loss_occurred <= 10'd1;
 			end
@@ -23,6 +25,8 @@ module lose_checker(
 				health_counter <= health_counter - 10'd1;
 			end
 		end
+		
+		previous_y <= ball_y;
 	end
 	
 endmodule 
